28f361c1330df4e83d8ea060e9067a77cb0ded22
pengkobe <yipeng.info@gmail.com>
2016-10-16 15:31:33 +0800

    fix chat-bar style

--- www/module/chat/js/directives/qqface/qqface.js
+++ www/module/chat/js/directives/qqface/qqface.js
@@ -10,7 +10,6 @@ angular.module('chat.directive')
             link: function(scope, element, attrs, controller) {
                 // 表情选择事件
                 scope.goListByTap = function(evt) {
-                    debugger;
                     if (evt.srcElement.title) {
                         var text_content = document.querySelector("#text_content");
                         var ret = text_content.value + "[" + event.srcElement.title + "]";
