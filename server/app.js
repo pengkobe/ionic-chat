b05a1701dfc30f04255b522fbc6779f353fe9bc1
pengkobe <yipeng.info@gmail.com>
2016-11-29 20:40:11 +0800

    config jwt...

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ var app = express();
 //设置跨域访问
 app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With,Authorization");
     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
     res.header("X-Powered-By",' yipeng-nodejs')
     next();
